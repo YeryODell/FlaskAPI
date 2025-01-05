09bd2fc058f7c0748a9527f95e00639397d92499
Yery <yery.odell@gmail.com>
2025-01-05 06:48:46 -0500

    corrected typo in comment

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ def get_movie(movie_title):
     responses = get_page_links(soup)
     print(type(responses))
     status_code = 200
-    # check to make sure a dictionary was returned
+    # check to make sure that something was returned
     if responses == {}:
         status_code = 204
 
